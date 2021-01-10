30101a3fd3b158653b77a1e451a9d65a403a9b63
Ram183 <ramcrazy1992@gmail.com>
2021-01-10 17:07:12 +0530

    Updated

--- src/org/sample/Client.java
+++ src/org/sample/Client.java
@@ -9,5 +9,10 @@ public class Client {
 	public void clientName() {
 		System.out.println("Client Name: Air canada");
 	}
+	
+	public void clientBloodGroup() {
+
+		System.out.println("Client Blood Group : O+Ve");
+	}
 
 }
